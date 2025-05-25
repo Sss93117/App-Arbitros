f964eff7af17f61e797221099ccc17ef91ce578c
Sss93117 <sandrosanchez091@gmail.com>
2025-05-26 01:47:27 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/App-Arbitros/', // Cambia "App-Arbitros" por el nombre de tu repo de GitHub
+  base: '/AppArbitrosMN/', // 
   plugins: [react()],
 })
